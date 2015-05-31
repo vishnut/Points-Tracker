59b57e65cd0766be38f51167e8e5332ae9794f5a
Vishnu Thiagarajan <Vishnu@Vishnus-MacBook-Air.local>
2015-05-31 16:28:16 -0500

    Initial Commit

---
+++ Points Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Points Tracker
+//
+//  Created by Vishnu Thiagarajan on 5/31/15.
+//  Copyright (c) 2015 Vishnu Thiagarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
