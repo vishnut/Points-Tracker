8a59a959771168f4edb2f2be99e5eb514bc08150
Vishnu Thiagarajan <Vishnu@Vishnus-MacBook-Air.local>
2016-10-09 13:09:49 -0500

    updates

--- Points Tracker/SettingsViewController.swift
+++ Points Tracker/SettingsViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var nameShow: UISwitch!
+    @IBOutlet weak var player1name: UITextField!
+    @IBOutlet weak var player2name: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var storage: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var check=storage.objectForKey("ns")
+        if(check != nil)
+        {
+        nameShow.on=check as! Bool
+        }
+        player1name.text=storage.objectForKey("p1name") as? String
+        player2name.text=storage.objectForKey("p2name") as? String
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +32,25 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func doneclicked(sender: UIButton) {
+        var storage: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        storage.setObject(nameShow.on, forKey: "ns")
+        if(nameShow.on)
+        {
+            if(player1name != nil){
+            storage.setObject(player1name.text, forKey: "p1name")
+            }
+            if(player2name != nil){
+            storage.setObject(player2name.text, forKey: "p2name")
+            }
+        }
+        else
+        {
+            storage.setObject("", forKey: "p1name")
+            storage.setObject("", forKey: "p2name")
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
--- Points Tracker/ViewController.swift
+++ Points Tracker/ViewController.swift
@@ -5,14 +5,44 @@
 //  Created by Vishnu Thiagarajan on 5/31/15.
 //  Copyright (c) 2015 Vishnu Thiagarajan. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var player1score: UILabel!
+    @IBOutlet weak var player2score: UILabel!
+    
+    var p1score:Int=0
+    var p2score:Int=0
+    
+    @IBOutlet weak var p1label: UILabel!
+    @IBOutlet weak var p2label: UILabel!
+    @IBOutlet weak var p1winning: UILabel!
+    @IBOutlet weak var p2winning: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var storage: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var s1:Int?=storage.objectForKey("1score") as? Int
+        var s2:Int?=storage.objectForKey("2score") as? Int
+        var n1:String? = (storage.objectForKey("p1name") as? String)
+        var n2:String? = (storage.objectForKey("p2name") as? String)
+        if(n1 != nil)
+        {
+            p1label.text=n1
+        }
+        if(n2 != nil)
+        {
+            p2label.text=n2
+        }
+        if(s1 != nil && s2 != nil)
+        {
+            p1score=s1!
+            p2score=s2!
+        }
+        applychanges()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressReset(sender: UIButton) {
+        p1score=0
+        p2score=0
+        applychanges()
+    }
+    
+    func applychanges(){
+        player1score.text=String(p1score)
+        player2score.text=String(p2score)
+        if p1score==p2score{
+            p1winning.text=""
+            p2winning.text=""
+        }
+        else if p1score>p2score{
+            p1winning.text="_________"
+            p2winning.text=""
+        }
+        else if p1score<p2score{
+            p1winning.text=""
+            p2winning.text="_________"
+        }
+    }
+    
+    @IBAction func p1press(sender: UIButton) {
+        p1score+=1
+        applychanges()
+    }
 
+    @IBAction func p2press(sender: UIButton) {
+        p2score+=1
+        applychanges()
+    }
+    @IBAction func accesssettings(sender: UIButton) {
+        var storage: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        storage.setObject(p1score, forKey: "1score")
+        storage.setObject(p2score, forKey: "2score")
+    }
 }
 
